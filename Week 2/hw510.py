15d794a75202ac69b9aa5326be8dd914538be543
rjbaicich <124328912+rjbaicich@users.noreply.github.com>
2023-05-10 21:23:46 -0400

    Add files via upload

---
+++ Week 2/hw510.py
@@ -0,0 +1,9 @@
+def ft_to_in(x):
+    return 12*x
+
+def Circum(rad):
+    return 2 * 3.14 * rad
+
+def square_footage(length, width):
+    square_footage = length * width
+    return square_footage
