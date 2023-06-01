6597cecbdb9f76f4348ea4d0faab1a2dc33053a7
rjbaicich <124328912+rjbaicich@users.noreply.github.com>
2023-05-31 22:06:02 -0400

    Add files via upload

---
+++ Week 5/movie_insert.sql
@@ -0,0 +1,56 @@
+INSERT INTO customer(
+    customer_id,
+    first_name
+)VALUES(
+    1,
+    'Randy'
+),
+(
+    2,
+    'John'
+);
+
+INSERT INTO tickets(
+    tic_id,
+    total_cost
+)VALUES(
+    11,
+    '12.50'
+),
+(
+    12,
+    '14.50'
+);
+
+INSERT INTO movies(
+    movie_id,
+    film_name,
+    genre
+)VALUES(
+    111,
+    'Bad Boys 2',
+    'Action'
+),
+(
+    122,
+    'Bronx Tale',
+    'Thriller'
+);
+
+INSERT INTO concession(
+    order_id,
+    oder_date,
+    sub_total,
+    total_cost,
+)VALUES(
+    1111,
+    '5-30',
+    '31.50',
+    '32.50'
+),
+(
+    1222,
+    '5-30',
+    '32.75'
+    '34.00'
+);
